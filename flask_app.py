3490d41c4248bf059dbcf78bf148ccab7f42b7e8
Bruno de Almeda Otero <b.otero@aluno.ifsp.edu.br>
2024-10-03 23:17:01 +0000

    initial commit

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,18 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask! Bem vindos alunos do IFSP'
+def index():
+    return render_template('index.html')
 
+@app.route('/user/<name>/<prontuary>/<institution>')
+def identification(name, prontuary, institution):
+    return render_template('identification.html', name=name, prontuary=prontuary, institution=institution)
+
+@app.route('/contextorequisicao')
+def requisition_context():
+    user_agent = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    host_app = request.host
+    return render_template('requisitionContext.html', user_agent=user_agent, remote_ip=remote_ip, host_app=host_app)
