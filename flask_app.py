779246f01b10397c119cd923a7241eece8b5e3c9
Bruno de Almeda Otero <b.otero@aluno.ifsp.edu.br>
2024-10-18 03:27:00 +0000

    initial commit

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
@@ -9,12 +9,19 @@ moment = Moment(app)
 bootstrap = Bootstrap(app)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<username>')
-def hello_user(username):
-    return render_template('index.html', username=username)
+@app.route('/user/<username>/<prontuary>/<institution>')
+def identification(username, prontuary, institution):
+    return render_template('identification.html', username=username, prontuary=prontuary, institution=institution)
+
+@app.route('/contextorequisicao/<username>')
+def requisitionContext(username):
+    user_agent = request.headers.get('User-Agent')
+    remote_ip = request.remote_addr
+    host_app = request.host
+    return render_template('requisitionContext.html', username=username, user_agent=user_agent, remote_ip=remote_ip, host_app=host_app)
 
 @app.errorhandler(404)
 def page_not_found(e):
