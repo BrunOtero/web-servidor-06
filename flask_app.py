2d7ac1230c86f1c7b0f300834878ab500db32731
Bruno de Almeda Otero <b.otero@aluno.ifsp.edu.br>
2024-10-25 00:37:51 +0000

    first commit

--- flask_app.py
+++ flask_app.py
@@ -1,27 +1,31 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+
+class NameForm(FlaskForm):
+  name = StringField('What is your name?', validators= [DataRequired()])
+  submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MY_S3CR37_K3Y'
 
 moment = Moment(app)
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-@app.route('/user/<username>/<prontuary>/<institution>')
-def identification(username, prontuary, institution):
-    return render_template('identification.html', username=username, prontuary=prontuary, institution=institution)
-
-@app.route('/contextorequisicao/<username>')
-def requisitionContext(username):
-    user_agent = request.headers.get('User-Agent')
-    remote_ip = request.remote_addr
-    host_app = request.host
-    return render_template('requisitionContext.html', username=username, user_agent=user_agent, remote_ip=remote_ip, host_app=host_app)
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.errorhandler(404)
 def page_not_found(e):
